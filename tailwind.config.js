f0ae966cb7544789ddcb68e1d197fa2d77b60dc0
Jorge Tercarioli <jordanter@gmail.com>
2025-05-27 18:29:07 +0200

    Primer commit - estructura base con Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#FF6B3D",
+        text: "#F6F8EB",
+        shadow: "#3E1104"
+      },
+      fontFamily: {
+        sans: ["'Work Sans'", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
